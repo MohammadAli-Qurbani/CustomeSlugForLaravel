05fe5185d3bae9114bcc71d668dcf75d96e0ab5d
mohamadaliqurbani <ali.eng.qurbani@gmail.com>
2024-09-09 12:58:33 +0430

    adding triming functionality and Farsi language support

--- slug.php
+++ slug.php
@@ -5,32 +5,34 @@ namespace App\CustomeSlug;
 trait CustomeSlug
 {
 
-    public static function myCustomeSlug(Object $model, String $SlugAble,bool $useForUpdate=false): string
+    public static function myCustomeSlug(Object $model, String $slugAable,bool $useForUpdate=false): string
     {
-        /**
-             *  - first searching or white space and replace with underscore
+         /**
+             *  - first searching for white space and replace with -
              *  - second search data slug in database
-             *  - thrid a record found take the last underscore and replace with a number plus one
+             *  - thrid a record found take the last - and replace with a number plus one
              *
          */
-        $SlugAble = str_replace(' ', '_', $SlugAble);
+        $slugAable = preg_replace('/\s+/', '-', trim($slugAable));
+        $slugAable = trim($slugAable, '-');
 
-        $searchedSlug = $model->where('slug','like',"%".$SlugAble."%")->latest('id')->first();
+        // Ensure proper handling of Farsi text by converting to UTF-8 if necessary
+        $slugAable = mb_convert_encoding($slugAable, 'UTF-8', 'auto');
+
+        $searchedSlug = $model->where('slug', 'like', "%$slugAable%")->latest('id')->first();
 
         if (!empty($searchedSlug)) {
-
-            if($useForUpdate && $SlugAble===$searchedSlug){
-                return $searchedSlug;
+            if ($useForUpdate && $slugAable === $searchedSlug->slug) {
+                return $searchedSlug->slug;
             }
-            $searchForSlugNumber = explode('_', $searchedSlug->slug);
 
-            $slugNumber = (int)end($searchForSlugNumber)+1;
+            $searchForSlugNumber = explode('-', $searchedSlug->slug);
+            $slugNumber = (int)end($searchForSlugNumber) + 1;
 
-            return $SlugAble . '_' . $slugNumber ;
-
+            return $slugAable . '-' . $slugNumber;
         }
 
-        return $SlugAble;
+        return $slugAable;
     }
 }
 
