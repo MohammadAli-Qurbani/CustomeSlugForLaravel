22bb26e674eb519a5e2ce7954b1ba478a903eb4f
mohamadaliqurbani <ali.eng.qurbani@gmail.com>
2024-09-09 14:35:29 +0430

    adding dynamic column of slug

--- slug.php
+++ slug.php
@@ -1,38 +1,33 @@
 <?php
-
-namespace App\CustomeSlug;
-
-trait CustomeSlug
-{
-
-    public static function myCustomeSlug(Object $model, String $slugAable,bool $useForUpdate=false): string
+namespace App\Services\Slug;
+class CustomeSlug{
+    public  function myCustomeSlug(Object $model,string $column, string $slugAable,bool $useForUpdate=false): string
     {
-         /**
+        /**
              *  - first searching for white space and replace with -
              *  - second search data slug in database
              *  - thrid a record found take the last - and replace with a number plus one
              *
          */
-        $slugAable = preg_replace('/\s+/', '-', trim($slugAable));
-        $slugAable = trim($slugAable, '-');
+        $trimedSlugAable = preg_replace('/\s+/', '-', trim($slugAable));
+        $trimedSlugAable = trim($trimedSlugAable, '-');
 
         // Ensure proper handling of Farsi text by converting to UTF-8 if necessary
-        $slugAable = mb_convert_encoding($slugAable, 'UTF-8', 'auto');
+        $trimedSlugAable = mb_convert_encoding($trimedSlugAable, 'UTF-8', 'auto');
 
-        $searchedSlug = $model->where('slug', 'like', "%$slugAable%")->latest('id')->first();
-
+        $searchedSlug = $model->where($column,"ILIKE","%$slugAable%")->orderBy('id','desc')->first();
+  
         if (!empty($searchedSlug)) {
-            if ($useForUpdate && $slugAable === $searchedSlug->slug) {
+            if ($useForUpdate && $trimedSlugAable === $searchedSlug->slug) {
                 return $searchedSlug->slug;
             }
 
             $searchForSlugNumber = explode('-', $searchedSlug->slug);
             $slugNumber = (int)end($searchForSlugNumber) + 1;
-
-            return $slugAable . '-' . $slugNumber;
+           
+            return $trimedSlugAable . '-' . $slugNumber;
         }
 
-        return $slugAable;
+        return $trimedSlugAable;
     }
 }
-
